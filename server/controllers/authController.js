95cc6464b2a41738f8f6bc2f5b5e336828daa3a9
Mohammed Ismail <m.ismail.dev.cs@gmail.com>
2026-09-24 22:45:00 +0200

    fix(server): correct env access variable to refresh

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,8 +33,8 @@ export const login = async (req, res) => {
     );
     const refreshToken = jwt.sign(
         { sub: user.id },
-        process.env.JWT_ACCESS_SECRET,
-        { expiresIn: process.env['JWT_ACCESS_EXPIRY' + rememberMe ? '_REMEMBER' : ''] }
+        process.env.JWT_REFRESH_SECRET,
+        { expiresIn: process.env['JWT_REFRESH_EXPIRY' + rememberMe ? '_REMEMBER' : ''] }
     );
     
     res.cookie(
