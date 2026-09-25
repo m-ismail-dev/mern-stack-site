3b30ca2ccea23a83c8f9eb0e9557bb92583fbf7b
Mohammed Ismail <m.ismail.dev.cs@gmail.com>
2026-09-25 17:06:32 +0200

    feat(server): implement auth routes

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { requireAuth } from '../middleware/auth';
+import { register, login, refresh, logout, me } from '../controllers/authController.js';
+
+const router = Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.post('/refresh', refresh);
+router.post('/logout', logout);
+router.get('/me', requireAuth, me);
+
+export default router;
