00d191dbe9a47d69973706d70b99992ac94f5160
Mohammed Ismail <m.ismail.dev.cs@gmail.com>
2026-09-25 16:36:40 +0200

    fix(server): use ../ for importing user model

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from './models/User.js';
+import User from '../models/User.js';
 
 
 const cookieBase = { httpOnly: true, sameSite: 'lax', secure: process.env.NODE_ENV === 'production' }
